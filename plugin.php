27e63bf30b16dc0a874015576297fc4b15085c92
David Cramer <david@digilab.co.za>
2016-08-30 17:16:15 +0200

    bootstrap UIX

--- plugin.php
+++ plugin.php
@@ -24,40 +24,24 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
+// Constants
 define('UIXV2_PATH',  plugin_dir_path( __FILE__ ) );
 define('UIXV2_CORE',  __FILE__ );
 define('UIXV2_URL',  plugin_dir_url( __FILE__ ) );
-define('UIXV2_VER',  '1.0.0' );
+define('UIXV2_VER',  '2.0.0' );
 
-/**
- * locate and find class via classes folder tructure.
- *
- * @since 2.0.0
- *
- * @param string $class     class name to be checked and autoloaded
- */
-function uixv2_autoload_class( $class ){
-    $parts = explode( '\\', $class );
-    $name = array_shift( $parts );
-    if( file_exists( UIXV2_PATH . 'classes/' . $name ) ){        
-        if( !empty( $parts ) ){
-            $name .= '/' . implode( '/', $parts );
-        }
-        $class_file = UIXV2_PATH . 'classes/' . $name . '.php';
-        if( file_exists( $class_file ) ){
-            include_once $class_file;
-        }
-    }
+/* If Composer */
+//autoload dependencies
+//require_once( DBPT_PATH . 'vendor/autoload.php' );
+/* END Composer */
+
+/* If UIX Installer */
+//Load UIX Bootstrap
+require_once( UIXV2_PATH . 'uix-bootstrap.php' );
+/* END UIX Installer */
+
+// Register UI folders.
+function uixv2_register_ui_folders( $uix ){
+    $uix->register( DBPT_PATH . 'includes/ui/' );
 }
-spl_autoload_register( 'uixv2_autoload_class', true, false );
-
-// bootstrap plugin load
-add_action( 'plugins_loaded', 'uixv2_plugin_bootstrap', 200 );
-function uixv2_plugin_bootstrap(){
-
-    // init UI
-    new \uixv2\ui();
-    // init plugin logic
-    new \uixv2\core();
-
-}
+add_filter( 'uix_register', 'uixv2_register_ui_folders' );
